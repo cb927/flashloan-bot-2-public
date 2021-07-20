e80a6941f87d2ffd177efd832c2554fd208a27f8
cb927 <75989799+cb927@users.noreply.github.com>
2021-07-20 15:02:26 -0700

    Initial upload

---
+++ contracts/TradingBot.sol
@@ -0,0 +1,122 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+interface Structs {
+    struct SwapDescription {
+        IERC20 srcToken;
+        IERC20 dstToken;
+        address srcReceiver;
+        address dstReceiver;
+        uint256 amount;
+        uint256 minReturnAmount;
+        uint256 guaranteedAmount;
+        uint256 flags;
+        address referrer;
+        bytes permit;
+    }
+
+    struct Val {
+        uint256 value;
+    }
+
+    enum ActionType {
+        Deposit, // supply tokens
+        Withdraw, // borrow tokens
+        Transfer, // transfer balance between accounts
+        Buy, // buy an amount of some token (externally)
+        Sell, // sell an amount of some token (externally)
+        Trade, // trade tokens against another account
+        Liquidate, // liquidate an undercollateralized or expiring account
+        Vaporize, // use excess tokens to zero-out a completely negative account
+        Call // send arbitrary data to an address
+    }
+
+    enum AssetDenomination {
+        Wei // the amount is denominated in wei
+    }
+
+    enum AssetReference {
+        Delta // the amount is given as a delta from the current value
+    }
+
+    struct AssetAmount {
+        bool sign; // true if positive
+        AssetDenomination denomination;
+        AssetReference ref;
+        uint256 value;
+    }
+
+    struct ActionArgs {
+        ActionType actionType;
+        uint256 accountId;
+        AssetAmount amount;
+        uint256 primaryMarketId;
+        uint256 secondaryMarketId;
+        address otherAddress;
+        uint256 otherAccountId;
+        bytes data;
+    }
+
+    struct Info {
+        address owner; // The address that owns the account
+        uint256 number; // A nonce that allows a single address to control many accounts
+    }
+
+    struct Wei {
+        bool sign; // true if positive
+        uint256 value;
+    }
+}
+
+contract DyDxPool is Structs {
+    function getAccountWei(Info memory account, uint256 marketId)
+        public
+        view
+        returns (Wei memory);
+
+    function operate(Info[] memory, ActionArgs[] memory) public;
+}
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
+ * the optional functions; to access them see `ERC20Detailed`.
+ */
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract DyDxFlashLoan is Structs {
+    // You can see full code in private repo
+}
+
+// SPDX-License-Identifier: MIT
+
+contract TradingBot is DyDxFlashLoan {
+    // You can see full code in private repo.
+}
---
+++ src/bot.js
@@ -0,0 +1,1 @@
+// You can see full code in privae repo.
